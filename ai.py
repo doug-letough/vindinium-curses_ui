0e78897081c261a8066fca87fec88d6ecfd81f99
Doug Le Tough <doug.letough@free.fr>
2014-10-09 22:44:45 +1100

    Code cleaning

--- ai.py
+++ ai.py
@@ -7,80 +7,82 @@
 #
 ########################################################################
 
-
 import random
 
+
 class AI:
+    """Pure random A.I, you may NOT use it to win ;-)"""
     def __init__(self):
         pass
 
     def process(self, game):
-        # Do whatever you need with the Game object game
+        """Do whatever you need with the Game object game"""
         self.game = game
-                                    
+
     def decide(self):
-        # must return a tuple containing in that order :
-        # 1 - path_to_goal  :
-        #         A list of coordinates representing the path to your 
-        #        bot's goal for this turn : 
-        #        - i.e: [(y, x) , (y, x), (y, x)] 
-        #        where y is the vertical position from top and x the 
-        #        horizontal position from left.
-        # 
-        # 2 - action :
-        #        A string that will be displayed in the 'Action' place.
-        #        - i.e : "Go to mine"
-        #
-        # 3 - decision :
-        #        A string containing what would be useful to understand
-        #        the choice you're bot has made and that will be printed
-        #        at the 'Decision' place.
-        #    
-        # 4- hero_move :
-        #        A string in one of the following : West, East, North, South, Stay
-        #
-        # 5 - nearest_enemy_pos :
-        #        A tuple containing the nearest enenmy position (see above)
-        #
-        # 6 - nearest_mine_pos :
-        #        A tuple containing the nearest enenmy position (see above)
-        #
-        # 7 - nearest_tavern_pos :
-        #        A tuple containing the nearest enenmy position (see above)
-        
+        """Must return a tuple containing in that order:
+          1 - path_to_goal :
+                  A list of coordinates representing the path to your
+                 bot's goal for this turn:
+                 - i.e: [(y, x) , (y, x), (y, x)]
+                 where y is the vertical position from top and x the
+                 horizontal position from left.
+
+          2 - action:
+                 A string that will be displayed in the 'Action' place.
+                 - i.e: "Go to mine"
+
+          3 - decision:
+                 A list of tuples containing what would be useful to understand
+                 the choice you're bot has made and that will be printed
+                 at the 'Decision' place.
+
+          4- hero_move:
+                 A string in one of the following: West, East, North,
+                 South, Stay
+
+          5 - nearest_enemy_pos:
+                 A tuple containing the nearest enenmy position (see above)
+
+          6 - nearest_mine_pos:
+                 A tuple containing the nearest enenmy position (see above)
+
+          7 - nearest_tavern_pos:
+                 A tuple containing the nearest enenmy position (see above)"""
+
         actions = ['mine', 'tavern', 'fight']
-        
-        decisions = {'mine': [("Mine", 30), ('Fight', 10), ('Tavern', 5)], \
-                    'tavern': [("Mine", 10), ('Fight', 10), ('Tavern', 50)], \
+
+        decisions = {'mine': [("Mine", 30), ('Fight', 10), ('Tavern', 5)], 
+                    'tavern': [("Mine", 10), ('Fight', 10), ('Tavern', 50)], 
                     'fight': [("Mine", 15), ('Fight', 30), ('Tavern', 10)]}
-        
+
         walkable = []
         path_to_goal = []
-        dirs = ["North", "East","South", "West", "Stay"]
-        
+        dirs = ["North", "East", "South", "West", "Stay"]
+
         for y in range(self.game.board_size):
             for x in range(self.game.board_size):
                 if (y, x) not in self.game.walls_locs or \
                     (y, x) not in self.game.taverns_locs or \
-                    (y, x) not in self.game.mines_locs :
+                    (y, x) not in self.game.mines_locs:
 
                     walkable.append((y, x))
-        
-        # With such a random path, path highlighting would 
+
+        # With such a random path, path highlighting would
         # display a random continuous line of red bullets over the map.
         first_cell = self.game.hero.pos
         path_to_goal.append(first_cell)
-        
+
         for i in range(int(round(random.random()*self.game.board_size))):
             for i in range(len(walkable)):
                 random.shuffle(walkable)
-                if (walkable[i][0] - first_cell[0] == 1 and \
-                    walkable[i][1] - first_cell[1] == 0) or \
-                    (walkable[i][1] - first_cell[1] == 1 and \
-                    walkable[i][0] - first_cell[0] == 0) :
-                        path_to_goal.append(walkable[i])
-                        first_cell = walkable[i]
-                        break
+                if (walkable[i][0] - first_cell[0] == 1 and 
+                        walkable[i][1] - first_cell[1] == 0) or \
+                        (walkable[i][1] - first_cell[1] == 1 and 
+                        walkable[i][0] - first_cell[0] == 0):
+                    path_to_goal.append(walkable[i])
+                    first_cell = walkable[i]
+                    break
 
         hero_move = random.choice(dirs)
         action = random.choice(actions)
@@ -88,16 +90,15 @@ class AI:
         nearest_enemy_pos = random.choice(self.game.heroes).pos
         nearest_mine_pos = random.choice(self.game.mines_locs)
         nearest_tavern_pos = random.choice(self.game.mines_locs)
-        
-            
-        return (path_to_goal, \
-                action,\
-                decision, \
-                hero_move, \
-                nearest_enemy_pos, \
-                nearest_mine_pos, \
+
+        return (path_to_goal, 
+                action, 
+                decision, 
+                hero_move, 
+                nearest_enemy_pos, 
+                nearest_mine_pos, 
                 nearest_tavern_pos)
-                    
+
 
 if __name__ == "__main__":
     pass
--- game.py
+++ game.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 class Hero:
-    """ The Hero object """
+    """The Hero object"""
     def __init__(self, hero):
-        try :
+        try:
             # Training bots have no elo or userId
             self.elo = hero['elo']
             self.user_id = hero['userId']
-        except :    
-            self.elo = 0    
+        except:
+            self.elo = 0
             self.user_id = 0
-                
+
         self.bot_id = hero['id']
         self.life = hero['life']
         self.gold = hero['gold']
@@ -22,9 +23,10 @@ class Hero:
         self.mines = []
         self.name = hero['name']
 
+
 class Game:
-    """ The game object that gather 
-    all game state informations """
+    """The game object that gather
+    all game state informations"""
     def __init__(self, state):
         self.state = state
         self.mines = {}
@@ -41,45 +43,39 @@ class Game:
         self.finished = None
         self.board_size = None
         self.board_map = []
-        
+
         self.process_data(self.state)
-    
+
     def process_data(self, state):
-        """ Parse the game state """
+        """Parse the game state"""
         self.set_url(state['viewUrl'])
         self.process_hero(state['hero'])
         self.process_game(state['game'])
 
-    
     def set_url(self, url):
-        """ Set the game object url var """
-        self.url = url            
-        
+        """Set the game object url var"""
+        self.url = url
+
     def process_hero(self, hero):
-        """ Process the hero data """
+        """Process the hero data"""
         self.hero = Hero(hero)
-        
-        
+
     def process_game(self, game):
-        """ Process the game data """
-        process = {'board': self.process_board, 
+        """Process the game data"""
+        process = {'board': self.process_board,
                     'heroes': self.process_heroes}
-                    
         self.turn = game['turn']
         self.max_turns = game['maxTurns']
         self.finished = game['finished']
-        
-        for key in game :
+        for key in game:
             if key in process:
                 process[key](game[key])
-    
-    
+
     def process_board(self, board):
-        """ Process the board datas
+        """Process the board datas
             - Retrieve walls locs, tavern locs
-            - Converts tiles in a displayable form """
+            - Converts tiles in a displayable form"""
         self.board_size = board['size']
-        
         tiles = board['tiles']
         map_line = ""
         char = None
@@ -88,8 +84,7 @@ class Game:
             for x in range(0, len(line), 2):
                 tile = line[x:x+2]
                 tile_coords = (y/self.board_size/2, x/2)
-
-                if tile [0] == " ":
+                if tile[0] == " ":
                     # It's passable
                     char = " "
                 elif tile[0] == "#":
@@ -102,9 +97,8 @@ class Game:
                     self.mines_locs.append(tile_coords)
                     self.mines[tile_coords] = tile[1]
                     if tile[1] == str(self.hero.bot_id):
-                        # This mine is belong to me :-)
+                        # This mine is belong to me:-)
                         self.hero.mines.append(tile_coords)
-                    
                 elif tile[0] == "[":
                     # It's a tavern
                     char = "T"
@@ -114,30 +108,24 @@ class Game:
                     char = "H"
                     if not int(tile[1]) == self.hero.bot_id:
                         # I don't want to be put in an array !
-                        # I'm not a number, i'm a free bot :-)
+                        # I'm not a number, i'm a free bot:-)
                         self.heroes_locs.append(tile_coords)
                     else:
                         # And I want to be differenciated
                         char = "@"
-                        
                 map_line = map_line + str(char)
             self.board_map.append(map_line)
             map_line = ""
 
     def process_heroes(self, heroes):
-        """ Add heroes """
+        """Add heroes"""
         for hero in heroes:
             self.spawn_points_locs[(hero['spawnPos']['y'], hero['spawnPos']['x'])] = hero['id']
             self.heroes.append(Hero(hero))
             # Add spawn points to map
             line = list(self.board_map[int(hero['spawnPos']['x'])])
             if line[int(hero['spawnPos']['y'])] != "@" and \
-                line[int(hero['spawnPos']['y'])]  != "H":
+                    line[int(hero['spawnPos']['y'])] != "H":
                 line[int(hero['spawnPos']['y'])] = "X"
             line = "".join(line)
             self.board_map[int(hero['spawnPos']['x'])] = line
-
-        
-                
-    
-        
